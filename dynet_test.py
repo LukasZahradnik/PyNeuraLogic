21bc51388001d607fdd028e63174be3e3b753ddf
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2021-01-31 23:23:07 +0100

    Add dynet test script

---
+++ dynet_test.py
@@ -0,0 +1,56 @@
+import os
+from dotenv import load_dotenv
+from neuralogic.settings import Settings
+from neuralogic.sources import Sources
+from neuralogic.builder import Model
+from neuralogic import initialize
+import dynet as dy
+
+load_dotenv()
+
+
+def train(neuralogic_path):
+    initialize(os.environ["CLASSPATH"])
+
+    settings = Settings()
+    sources = Sources.from_dir(neuralogic_path, settings)
+    model = Model.from_neuralogic(settings, sources)
+
+    import neuralogic.dynet as nldy
+
+    deserializer = nldy.DyNetDeserializer(model)
+
+    epochs = 10_000
+    trainer = dy.SimpleSGDTrainer(deserializer.model, learning_rate=0.1)
+    printouts = 10
+    seen_instances = 0
+    total_loss = 0
+
+    for iter in range(epochs):
+        if iter > 0 and iter % printouts == 0:
+            print(iter, " average loss is:", total_loss / seen_instances)
+
+        seen_instances = 0
+        total_loss = 0
+
+        for sample in model.samples:
+            dy.renew_cg(immediate_compute=False, check_validity=False)
+
+            label = dy.scalarInput(sample.target)
+            graph_output = deserializer.build_sample(sample)
+
+            loss = dy.squared_distance(graph_output, label)
+            total_loss += loss.value()
+            loss.backward()
+            trainer.update()
+            seen_instances += 1
+
+    for sample in model.samples:
+        dy.renew_cg(immediate_compute=False, check_validity=False)
+
+        graph_output = deserializer.build_sample(sample)
+        label = dy.scalarInput(sample.target)
+        print(f'label: {label.value()}, output: {graph_output.value()}')
+
+
+train("./dataset/molecules/mutagenesis")
