dcd2da6045349f514f2546280698b60bc979611f
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2021-02-21 19:06:47 +0100

    Update dgl example usage

--- dgl_test.py
+++ dgl_test.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from neuralogic.settings import Settings
 from neuralogic.sources import Sources
 from neuralogic.builder import Model
+from neuralogic.preprocessing import to_processed_samples
 from neuralogic import initialize
 from neuralogic.dgl import NeuraLogicLayer
 import torch.nn.functional as F
@@ -17,45 +18,42 @@ def train(neuralogic_path):
     settings = Settings()
 
     sources = Sources.from_dir(neuralogic_path, settings)
-
     neuralogic_model = Model.from_neuralogic(settings, sources)
 
     model = NeuraLogicLayer(neuralogic_model.weights)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     total_loss = 0
     seen_instances = 0
     printouts = 10
 
-    for iter in range(100):
+    samples = to_processed_samples(neuralogic_model.samples)
+
+    for iter in range(800):
         if iter > 0 and iter % printouts == 0:
             print(iter, " average loss is:", total_loss / seen_instances)
         seen_instances = 0
         total_loss = 0
 
-        for sample in neuralogic_model.samples:
-            # sample = neuralogic_model.samples[0]
-
+        for sample in samples:
             model.train()
 
-            label = torch.tensor([float(sample.target)])
+            label = torch.tensor([sample.target])
 
             optimizer.zero_grad()
             out = model(sample)
 
-            loss = F.mse_loss(torch.take(out, torch.tensor([0])), label)
+            loss = F.mse_loss(out, label)
             loss.backward()
             optimizer.step()
 
             seen_instances += 1
             total_loss += float(loss)
 
-    for sample in neuralogic_model.samples:
+    for sample in samples:
         output = model(sample)[0]
         label = sample.target
         print(f"label: {label}, output: {output}")
 
 
-train(
-    "./dataset/molecules/mutagenesis",
-)
+train("./dataset/simple/xor/naive")
