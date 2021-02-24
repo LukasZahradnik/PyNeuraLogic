25f51d9829b04cc169fd5c0bb4cfd45679823bca
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2021-02-24 22:39:18 +0100

    Update PyG implementation

--- neuralogic/pytorch.py
+++ neuralogic/pytorch.py
@@ -1,107 +1,93 @@
-from .builder import Weight, Sample
-from .utils import to_layers
+from neuralogic.builder import Weight
 from typing import List
 import torch
 from torch import Tensor
-from torch.nn import Parameter, ParameterList
+from torch.nn import Parameter, ParameterList, Sequential
 from torch_geometric.nn.conv import MessagePassing
-from torch_geometric.nn.inits import zeros
+from neuralogic.preprocessing import ProcessedSample, Layer
 
 
 class NeuraLogicHelperLayer(MessagePassing):
-    def __init__(self, activation, weights):
-        super(NeuraLogicHelperLayer, self).__init__(aggr=NeuraLogicLayer.aggregations.get(activation, "add"))
-
-        self.activation = NeuraLogicLayer.activations.get(activation, None)
-        self.weights = weights
-
-    def forward(self, layer_input):
-        x, neurons = layer_input
-
-        from_neuron = []
-        to_neuron = []
-        edge_weight = []
-
-        for neuron in neurons:
-            if neuron.weights is None:
-                edge_weight = None
-                for input in neuron.inputs:
-                    from_neuron.append(input)
-                    to_neuron.append(neuron.index)
-            else:
-                for input, weight in zip(neuron.inputs, neuron.weights):
-                    from_neuron.append(input)
-                    to_neuron.append(neuron.index)
-                    edge_weight.append(self.weights[weight + 1])
-
-        if len(from_neuron) == 0:
-            return x
-
-        edge_index = torch.tensor([from_neuron, to_neuron])
-        out = self.propagate(edge_index, x=x, edge_weight=edge_weight)
-
-        if self.activation:
-            out = self.activation(out)
-
-        return x + out
-
-    def message(self, x_j: Tensor, edge_weight=None) -> Tensor:
-        out = x_j
-
-        if edge_weight is None:
-            return out
-
-        output = []
-
-        for x, w in zip(out, edge_weight):
-            value = x * w
-            output.append(value)
-
-        return torch.stack(output)
-
-
-class NeuraLogicLayer(torch.nn.Module):
     activations = {
         "Sigmoid": torch.sigmoid,
         "ReLu": torch.relu,
         "Tanh": torch.tanh,
+        "Average": lambda x: torch.mean(x, 1, True),
+        "Maximum": lambda x: torch.max(x, 1, True),
     }
 
-    aggregations = {
-        "Average": "mean",
-        "Maximum": "max",
-    }
+    def __init__(self, layer: Layer, neuron_count: int, weights: ParameterList):
+        super(NeuraLogicHelperLayer, self).__init__()
 
+        self.reduce = "Sum" if layer.activation not in NeuraLogicHelperLayer.activations else layer.activation
+
+        self.neuron_count = neuron_count
+        self.layer = layer
+        self.weights = weights
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if len(self.layer.u) == 0:
+            return x
+
+        old_x = x.clone()
+        edge_index = torch.tensor([self.layer.u, self.layer.v])
+
+        if self.layer.has_weights:
+            print(x.shape)
+            out = self.propagate(edge_index, x=x)
+        else:
+            out = x
+
+        if self.reduce != "Sum":
+            out = self.reduce_function(out)
+
+        if len(old_x.shape) < len(out):
+            old_x = old_x.reshape(old_x.shape + tuple(1 for _ in range(len(out.shape) - len(old_x.shape))))
+        return old_x
+
+    def reduce_function(self, x):
+        return self.activations[self.reduce](x)
+
+    def message(self, x_j: Tensor) -> Tensor:
+        out = [
+            (self.weights[w] @ x) if x.shape != (1,) and self.weights[w].shape != (1,) else (x * self.weights[w])
+            for x, w in zip(x_j, self.layer.weights)
+        ]
+
+        return torch.stack(out)
+
+
+class NeuraLogicLayer(torch.nn.Module):
     def __init__(self, weights: List[Weight]):
         super(NeuraLogicLayer, self).__init__()
 
         params = [
-            Parameter(torch.tensor(weight.dimensions, dtype=torch.float), requires_grad=not weight.fixed)
+            Parameter(
+                torch.tensor(weight.value, dtype=torch.float).reshape(weight.dimensions), requires_grad=not weight.fixed
+            )
             for weight in weights
         ]
 
-        self.built_layers = False
-        self.neura_layers: List[NeuraLogicHelperLayer] = []
         self.weights = ParameterList(params)
+        self.meta_weights = weights
+        self.reset_parameters()
 
-        self.reset_params()
+    def reset_parameters(self):
+        for weight, meta_weight in zip(self.weights, self.meta_weights):
+            if not meta_weight.fixed:
+                torch.nn.init.uniform_(weight)
 
-    def reset_params(self):
-        self.weights[0][0] = 1
+    def forward(self, sample: ProcessedSample):
+        neuron_count = len(sample.neurons)
 
-        for weight in self.weights[1:]:
-            zeros(weight)
+        neural_layers = [NeuraLogicHelperLayer(layer, neuron_count, self.weights) for layer in sample.layers]
+        model = Sequential(*neural_layers)
 
-    def forward(self, sample: Sample):
-        layers = to_layers(sample)
+        x = torch.zeros((neuron_count, 3, 1))
 
-        if not self.built_layers:
-            self.built_layers = True
-            self.neura_layers = [NeuraLogicHelperLayer(layer[0].activation, self.weights) for layer in layers]
+        for neuron_index in sample.layers[0].targets:
+            neuron = sample.neurons[neuron_index]
+            x[neuron.index].fill_(neuron.value)
 
-        x = torch.zeros((len(sample.neurons), 1))
-        x[layers[0][0].index] = 1
-
-        for n_layer, layer in zip(self.neura_layers, layers):
-            x = n_layer((x, layer))
-        return x[layers[-1][0].index]
+        x = model(x)
+        return torch.take(x, torch.tensor([sample.neurons[-1].index]))
