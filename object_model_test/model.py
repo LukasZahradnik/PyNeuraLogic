1b3ccc2da1ed7f009790ad3c207b6271a4355e74
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2021-03-25 22:13:52 +0100

    Change implementation of writing rules with objects

--- object_model_test/model.py
+++ object_model_test/model.py
@@ -1,99 +1,220 @@
-from typing import Iterable, Generator
+from typing import Iterable, Union, Dict
 
 
-class AtomBase:
-    def __init__(self, name):
-        self.name = name
-        self.children = None
-        self.can_have_child = True
+AtomType = Union["BaseAtom", "WeightedAtom"]
+BodyAtomType = Union["BaseAtom", "WeightedAtom"]
 
-    def __call__(self, *args) -> "RuleAtom":
-        return RuleAtom(self.name, args)
-
-    def __getitem__(self, item) -> "RuleAtom":
-        return RuleAtom(self.name, None)[item]
-
-    def __gt__(self, other) -> "RuleAtom":
-        atom = RuleAtom(self.name)
-        atom.can_have_child = self.can_have_child
-        return atom > other
-
-    def __str__(self) -> str:
-        if self.children is None:
-            return f"{self.name}."
-        return self.name
+Head = AtomType
+Body = Union[Iterable[BodyAtomType], BodyAtomType]
 
 
-class RuleAtom(AtomBase):
-    def __init__(self, name, args=None):
-        super().__init__(name)
+class Rule:
+    def __init__(self, head: Head, body: Body):
+        self.head = head
 
-        self.args = args
-        self.weights = None
+        if not isinstance(body, Iterable):
+            body = [body]
+        self.body = list(body)
 
-    def __call__(self, *args, **kwargs):
-        raise NotImplementedError
+    def __str__(self):
+        return f"{self.head.to_str()} :- {', '.join(atom.to_str() for atom in self.body)}."
 
-    def __getitem__(self, item) -> "RuleAtom":
-        self.weights = item
+    def __and__(self, other):
+        if isinstance(other, Iterable):
+            self.body.extend(list(other))
+        else:
+            self.body.append(other)
         return self
 
-    def __le__(self, other) -> "RuleAtom":
-        if not self.can_have_child:
-            raise NotImplementedError
 
-        if isinstance(other, (RuleAtom, AtomBase)):
-            other.can_have_child = False
-            other.children = None
+class Conjunction:
+    def __init__(self):
+        self.body = []
+        self.n = 0
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n < len(self.body):
+            self.n += 1
+            return self.body[self.n]
+        else:
+            raise StopIteration
+
+    def __rand__(self, other):
+        if isinstance(other, (WeightedAtom, BaseAtom)):
+            self.body.append(other)
+        elif isinstance(other, Conjunction):
+            self.body.extend(other.body)
         elif isinstance(other, Iterable):
-            if isinstance(other, Generator):
-                other_store = []
-
-                for atom in other:
-                    atom.can_have_child = False
-                    atom.children = None
-                    other_store.append(atom)
-                other = other_store
-            else:
-                for atom in other:
-                    atom.can_have_child = False
-                    atom.children = None
-        self.children = other
+            self.body.extend(list(other))
         return self
 
+
+class BaseAtom:
+    """ Atom """
+
+    def __init__(self, predicate: "Predicate", weight=None, terms=None, arity=0, negated=False):
+        self.predicate = predicate
+        self.arity = arity
+
+        self.negated = negated
+        self.terms = terms
+        self.weight = weight
+
+        if self.weight is None:
+            self.weight = []
+        elif not isinstance(self.weight, Iterable):
+            self.weight = [self.weight]
+
+        if self.terms is None:
+            self.terms = []
+        elif not isinstance(self.terms, Iterable):
+            self.terms = [self.terms]
+
+        if len(self.terms) != arity:
+            print(arity, len(self.terms), self.terms)
+            raise Exception
+
+    def __neg__(self) -> "BaseAtom":
+        return self.__invert__()
+
+    def __invert__(self) -> "BaseAtom":
+        return BaseAtom(self.predicate, self.weight, self.terms, self.arity, not self.negated)
+
+    def __call__(self, *args) -> "BaseAtom":
+        if self.terms:
+            raise Exception
+
+        arity = 1
+
+        if isinstance(args, Iterable):
+            terms = list(args)
+            arity = len(terms)
+        else:
+            terms = args
+        return BaseAtom(self.predicate, self.weight, terms, arity, self.negated)
+
+    def __getitem__(self, item) -> "WeightedAtom":
+        return WeightedAtom(self, item)
+
+    def __le__(self, other: Body) -> Rule:
+        return Rule(self, other)
+
+    def to_str(self, end=False) -> str:
+        negation = "~" if self.negated else ""
+        end = "." if end else ""
+
+        if self.terms:
+            terms = ", ".join(str(term) for term in self.terms)
+            return f"{negation}{self.predicate.to_str()}({terms}){end}"
+        return f"{negation}{self.predicate.to_str()}{end}"
+
     def __str__(self) -> str:
-        children = ""
-        weights = ""
-        args = ""
+        return self.to_str(True)
 
-        if self.children is not None:
-            if isinstance(self.children, RuleAtom):
-                children = f" :- {self.children}"
-            elif isinstance(self.children, Iterable):
-                children = f" :- {', '.join(str(child) for child in self.children)}"
-            else:
-                raise Exception
 
-        if self.args is not None:
-            if isinstance(self.args, str):
-                args = f"({self.args})"
-            elif isinstance(self.args, Iterable):
-                args = f"({', '.join(str(x) for x in self.args)})"
+class Predicate:
+    """WeightedPredicate"""
 
-        if self.weights is not None:
-            if isinstance(self.weights, (str, int)):
-                weights = f"{{{self.weights}}} "
-            elif isinstance(self.weights, Iterable):
-                weights = f"{{{', '.join(str(x) for x in self.weights)}}} "
+    def __init__(self, name, arity, private, special):
+        self.name = name
+        self.arity = arity
+        self.private = private
+        self.special = special
 
-        if self.can_have_child:
-            return f"{weights}{self.name}{args}{children}."
-        return f"{weights}{self.name}{args}{children}"
+    def set_arity(self, arity):
+        if self.arity == arity:
+            return self
+
+    def to_str(self):
+        special = "@" if self.special else ""
+        private = "*" if self.private else ""
+        return f"{private}{special}{self.name}"
+
+    def __str__(self):
+        special = "@" if self.special else ""
+        private = "*" if self.private else ""
+        return f"{private}{special}{self.name}/{self.arity}"
+
+
+class WeightedAtom:
+    """ValuedFact"""
+
+    def __init__(self, atom: BaseAtom, weight, fixed=False):
+        self.atom = atom
+        self.weight = weight
+        self.is_fixed = fixed
+
+        if isinstance(weight, Iterable) and not isinstance(weight, tuple):
+            self.weight = list(weight)
+
+    def fixed(self) -> "WeightedAtom":
+        if self.is_fixed:
+            raise Exception
+        return WeightedAtom(self.atom, self.weight, True)
+
+    def __invert__(self) -> "WeightedAtom":
+        self.atom = ~self.atom
+        return self
+
+    def __neg__(self) -> "WeightedAtom":
+        return self.__invert__()
+
+    def __le__(self, other: Body) -> Rule:
+        return Rule(self, other)
+
+    def to_str(self, end=False):
+        if isinstance(self.weight, tuple):
+            weight = f"{{{', '.join(str(w) for w in self.weight)}}}"
+        else:
+            weight = str(self.weight)
+
+        if self.is_fixed:
+            return f"<{weight}> {self.atom.to_str(end)}"
+        return f"{weight} {self.atom.to_str(end)}"
+
+    def __str__(self):
+        return self.to_str(True)
 
 
 class AtomFactory:
-    def __getattr__(self, item) -> AtomBase:
-        return AtomBase(item)
+    class Predicate:
+        predicates: Dict[str, Predicate] = {}
+
+        def __init__(self, private=False, special=False):
+            self.is_private = private
+            self.is_special = special
+
+        @property
+        def special(self) -> "AtomFactory.Predicate":
+            return AtomFactory.Predicate(self.is_private, True)
+
+        @property
+        def private(self) -> "AtomFactory.Predicate":
+            return AtomFactory.Predicate(True, self.is_special)
+
+        def __getattr__(self, item):
+            key = f"{item}/0"
+            if key not in AtomFactory.Predicate.predicates:
+                AtomFactory.Predicate.predicates[key] = Predicate(item, 0, self.is_private, self.is_special)
+            predicate = AtomFactory.Predicate.predicates[key]
+            return BaseAtom(predicate)
+
+    def __init__(self):
+        self.instances: Dict[str, Dict[int, BaseAtom]] = {}
+
+        self.special = AtomFactory.Predicate(special=True)
+        self.private = AtomFactory.Predicate(private=True)
+
+    def __getattr__(self, item) -> BaseAtom:
+        key = f"{item}/0"
+        if key not in AtomFactory.Predicate.predicates:
+            AtomFactory.Predicate.predicates[key] = Predicate(item, 0, False, False)
+        predicate = AtomFactory.Predicate.predicates[key]
+        return BaseAtom(predicate)
 
 
 class VariableFactory:
@@ -101,5 +222,11 @@ class VariableFactory:
         return item.upper()
 
 
+class TermFactory:
+    def __getattr__(self, item) -> str:
+        return item.lower()
+
+
 Var = VariableFactory()
 Atom = AtomFactory()
+Term = TermFactory()
