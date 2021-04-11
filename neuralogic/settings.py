6fb256ae01c5417cffe16ad142faf309a739d940
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2021-04-11 18:10:38 +0200

    Update settings

--- neuralogic/settings.py
+++ neuralogic/settings.py
@@ -1,29 +1,39 @@
-from . import get_neuralogic
-from py4j.java_gateway import set_field
+from neuralogic import get_neuralogic, get_gateway
+from py4j.java_gateway import set_field, get_field
 
 
 class Settings:
     def __init__(self):
         self.namespace = get_neuralogic().cz.cvut.fel.ida.setup
         self.settings = self.namespace.Settings()
+
+        set_field(self.settings, "debugExporting", False)
         set_field(self.settings, "isoValueCompression", False)
         # set_field(self.settings, "chainPruning", False)
 
     @property
     def seed(self) -> int:
-        return self.settings.seed
+        return get_field(self.settings, "seed")
 
     @seed.setter
     def seed(self, seed: int):
-        self.settings.seed = seed
+        set_field(self.settings, "seed", seed)
+
+    @property
+    def debug_exporting(self) -> bool:
+        return get_field(self.settings, "debugExporting")
+
+    @debug_exporting.setter
+    def debug_exporting(self, debug_export: bool):
+        set_field(self.settings, "debugExporting", debug_export)
 
     @property
     def default_fact_value(self) -> float:
-        return self.settings.defaultFactValue
+        return get_field(self.settings, "defaultFactValue")
 
     @default_fact_value.setter
     def default_fact_value(self, value: float):
-        self.settings.defaultFactValue = value
+        set_field(self.settings, "defaultFactValue", value)
 
     def to_json(self) -> str:
         return self.settings.exportToJson()
