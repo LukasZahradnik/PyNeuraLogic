dad8fda7a004398f215c6673b40651156957c3be
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2021-04-28 00:35:26 +0200

    Add possible manual backprop to java and faster iterations

--- neuralogic/nn/evaluators/java.py
+++ neuralogic/nn/evaluators/java.py
@@ -1,45 +1,49 @@
 from typing import Optional
 
-from neuralogic.builder import Backend
-from neuralogic.data import Dataset
-from neuralogic.model import Model
+from neuralogic.core.builder import Backend
+from neuralogic.core import Problem
+from neuralogic.core.model import Model
 from neuralogic.nn.java import NeuraLogicLayer
 from neuralogic.nn.base import AbstractEvaluator
-from neuralogic.settings import Settings
+from neuralogic.core.settings import Settings
+from neuralogic.utils.data import Dataset
 
 
 class JavaEvaluator(AbstractEvaluator):
-    def __init__(self, model: Optional[Model], dataset: Optional[Dataset], settings: Settings):
-        super().__init__(model, dataset, settings)
+    def __init__(
+        self, problem: Optional[Problem], model: Optional[Model], dataset: Optional[Dataset], settings: Settings
+    ):
+        super().__init__(problem, model, dataset, settings)
 
-        if model is not None:
-            self.dataset = model.build(Backend.JAVA)
-        if dataset is not None:
+        if problem is not None:
+            model, dataset = problem.build(Backend.JAVA)
+
             self.dataset = dataset
-        self.neuralogic_layer = NeuraLogicLayer(self.dataset)
+            self.model = model
 
-    def train(self, generator: bool = True):
-        epochs = self.settings.epochs
+        self.neuralogic_layer = NeuraLogicLayer(self.model)
+        self.neuralogic_layer.set_training_samples(self.dataset.samples)
+
+    def train(self, generator: bool = True, epochs: int = None):
+        if epochs is None:
+            epochs = self.settings.epochs
 
         def _train():
             for _ in range(epochs):
-                result = self.neuralogic_layer(self.dataset.samples, True)
+                result = self.neuralogic_layer(None, True)
                 yield result
 
         if generator:
             return _train()
 
-        stats = 0, 0
-        for stats in _train():
-            pass
-        return stats
+        return self.neuralogic_layer(None, True, epochs=epochs)
 
     def test(self, generator: bool = True):
         def _test():
             for sample in self.dataset.samples:
                 result = self.neuralogic_layer(sample, False)
-                yield result
+                yield result.target(), result.output()
 
         if generator:
             return _test()
-        return list(_test())
+        return self.neuralogic_layer(self.dataset.samples, False)
--- neuralogic/nn/java.py
+++ neuralogic/nn/java.py
@@ -1,28 +1,43 @@
-from collections import Iterable
+from collections import Sized
 
-from neuralogic import get_neuralogic, get_gateway
+from neuralogic import get_neuralogic
 from py4j.java_gateway import get_field
-from py4j.java_collections import ListConverter
 
-from neuralogic.data import Dataset
+from neuralogic.core.model import Model
+
+
+class Loss:
+    def __init__(self, loss):
+        self.loss = loss
+
+    def backward(self):
+        self.loss.backward()
+
+    def value(self) -> float:
+        return get_field(self.loss.getError(), "value")
+
+    def output(self) -> float:
+        return get_field(self.loss.getOutput(), "value")
+
+    def target(self) -> float:
+        return get_field(self.loss.getTarget(), "value")
 
 
 class NeuraLogicLayer:
-    def __init__(self, dataset: Dataset):
+    def __init__(self, model: Model):
         self.namespace = get_neuralogic().cz.cvut.fel.ida.neural.networks.computation.training.strategies
         self.do_train = True
-        self.settings = dataset.settings
+        self.settings = model.settings
 
-        self.neural_model = dataset.neural_model
-        self.strategy = self.namespace.IterativeTrainingStrategy(
-            dataset.settings.settings, dataset.neural_model, ListConverter().convert([], get_gateway()._gateway_client)
-        )
-        self.trainer = get_field(self.strategy, "trainer")
+        self.neural_model = model.model
+        self.strategy = self.namespace.PythonTrainingStrategy(model.settings.settings, model.model)
+
+        self.samples_len = 0
+
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.trainer.restart(self.settings.settings)
-        self.neural_model.resetWeights(get_field(self.strategy, "valueInitializer"))
+        self.strategy.resetParameters()
 
     def train(self):
         self.do_train = True
@@ -30,21 +45,29 @@ class NeuraLogicLayer:
     def test(self):
         self.do_train = False
 
-    def __call__(self, samples, train: bool = None):
+    def set_training_samples(self, samples):
+        self.samples_len = len(samples)
+        self.strategy.setSamples(samples)
+
+    def __call__(self, samples=None, train: bool = None, auto_backprop: bool = False, epochs: int = 1):
         if train is not None:
             self.do_train = train
 
-        iterable = True
+        if samples is None:
+            results = self.strategy.learnSamples(epochs)
+            return sum(get_field(result.errorValue(), "value") for result in results), self.samples_len
 
-        if not isinstance(samples, Iterable):
-            iterable = False
-            samples = ListConverter().convert([samples], get_gateway()._gateway_client)
+        if not isinstance(samples, Sized):
+            if self.do_train:
+                if auto_backprop:
+                    result = self.strategy.learnSample(samples)
+                    return get_field(result.errorValue(), "value"), 1
+            result = self.strategy.evaluateSample(samples)
+            return Loss(result)
 
         if self.do_train:
-            results = self.trainer.learnEpoch(self.neural_model, samples)
+            results = self.strategy.learnSamples(samples, epochs)
             return sum(get_field(result.errorValue(), "value") for result in results), len(samples)
 
-        results = self.trainer.evaluate(samples)
-        if iterable is False:
-            return get_field(results[0].getTarget(), "value"), get_field(results[0].getOutput(), "value")
+        results = self.strategy.evaluateSamples(samples)
         return [(get_field(result.getTarget(), "value"), get_field(result.getOutput(), "value")) for result in results]
