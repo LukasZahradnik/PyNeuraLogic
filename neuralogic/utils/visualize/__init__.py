6ce9a664e5a51f49e15918afc1a6f223057a162b
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2021-05-19 23:58:30 +0200

    Add drawing img type settings and dot source getter

--- neuralogic/utils/visualize/__init__.py
+++ neuralogic/utils/visualize/__init__.py
@@ -6,28 +6,48 @@ from neuralogic.core.settings import Settings
 from py4j.java_gateway import set_field
 
 
-def get_template_drawer():
-    namespace = get_neuralogic().cz.cvut.fel.ida.pipelines.debugging.drawing
-
+def get_drawing_settings(img_type="png"):
     settings = Settings()
 
     set_field(settings.settings, "drawing", False)
     set_field(settings.settings, "storeNotShow", True)
+    set_field(settings.settings, "imgType", img_type)
+
+    return settings
+
+
+def get_template_drawer(settings: Settings):
+    namespace = get_neuralogic().cz.cvut.fel.ida.pipelines.debugging.drawing
 
     return namespace.TemplateDrawer(settings.settings)
 
 
-def draw_model(model, filename: Optional[str] = None, draw_ipython=True, *args, **kwargs):
+def draw_model(model, filename: Optional[str] = None, draw_ipython=True, img_type="png", *args, **kwargs):
     if model.need_sync:
         model.sync_template()
 
     template = model.template
-    template_drawer = get_template_drawer()
+    template_drawer = get_template_drawer(get_drawing_settings(img_type=img_type))
 
-    data: bytes = template_drawer.drawForPython(template, filename)
+    if filename is not None:
+        template_drawer.drawIntoFile(template, filename)
 
-    if filename is None and draw_ipython:
+        return
+
+    data = template_drawer.drawIntoBytes(template)
+
+    if draw_ipython:
         from IPython.display import Image
 
         return Image(data, *args, **kwargs)
     return data
+
+
+def model_to_dot_source(model) -> str:
+    if model.need_sync:
+        model.sync_template()
+
+    template = model.template
+    template_drawer = get_template_drawer(get_drawing_settings())
+
+    return template_drawer.getGraphSource(template)
