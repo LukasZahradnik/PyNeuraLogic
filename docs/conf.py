e88aef0e0308ca77b0a6c0ccc62a88456034dbd1
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2022-03-26 23:28:25 +0100

    Fix docs napoleon extension

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx_rtd_theme",
     "sphinx_tabs.tabs",
-    "sphinxcontrib.napoleon",
+    "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
 ]
 
