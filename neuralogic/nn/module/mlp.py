9c602ec5af8ee691ccaf02958c65dd9d7f1d7778
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2022-04-22 17:53:43 +0200

    Fix MLP module

--- neuralogic/nn/module/mlp.py
+++ neuralogic/nn/module/mlp.py
@@ -48,19 +48,17 @@ class MLP(Module):
 
         for index, (in_channels, out_channels) in enumerate(zip(self.units[:-1], self.units[1:])):
             out_layer = R.get(f"{self.output_name}__{index}")
-            layers.append(out_layer(V.I)[out_channels, in_channels] <= prev_layer(V.I))
 
-            if isinstance(self.activation, list):
-                layers.append(out_layer / 1 | Metadata(activation=self.activation[index]))
-            else:
-                layers.append(out_layer / 1 | metadata)
+            layers.append(out_layer(V.I)[out_channels, in_channels] <= prev_layer(V.I))
+            act_layer = out_layer / 1 | (Metadata(activation=self.activation[index]) if metadata is None else metadata)
+
+            layers.append(act_layer)
             prev_layer = out_layer
 
         in_channels, out_channels = self.units[-2], self.units[-1]
-        layers.append(R.get(self.output_name)[out_channels, in_channels] <= prev_layer(V.I))
+        out = R.get(self.output_name)
 
-        if isinstance(self.activation, list):
-            layers.append(R.get(self.output_name) / 1 | Metadata(activation=self.activation[-1]))
-        else:
-            layers.append(R.get(self.output_name) / 1 | metadata)
+        layers.append(out(V.I)[out_channels, in_channels] <= prev_layer(V.I))
+        layers.append(out / 1 | (Metadata(activation=self.activation[-1]) if metadata is None else metadata))
+
         return layers
