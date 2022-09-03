094f8011b167b207b79531347d2b7a82b095d932
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2022-09-03 23:06:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -60,13 +60,15 @@ setup(
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
+        "Intended Audience :: Education",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
