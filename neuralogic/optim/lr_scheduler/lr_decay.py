f2ff321befc5fa360b5b1cd568e6620d25b38f9f
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2022-11-02 20:50:18 +0100

    Expose lr decay restarting

--- neuralogic/optim/lr_scheduler/lr_decay.py
+++ neuralogic/optim/lr_scheduler/lr_decay.py
@@ -6,6 +6,14 @@ class LRDecay:
     def _initialize(self, learning_rate):
         raise NotImplementedError
 
+    def _initialize_optimizer(self):
+        if self._decay is not None:
+            return
+        if self._optimizer is None:
+            raise ValueError("LRDecay cannot be initialized - no optimizer has been attached")
+        self._optimizer.initialize()
+        self._initialize(self._optimizer._lr_object)
+
     def decay(self, epoch: int):
         """
         Manually run the learning rate decay - this is useful when passing sample by sample into the training method
@@ -18,9 +26,12 @@ class LRDecay:
         epoch : int
             The number of the current epoch.
         """
-        if self._decay is None:
-            if self._optimizer is None:
-                raise ValueError("LRDecay cannot be initialized - no optimizer has been attached")
-            self._optimizer.initialize()
-            self._initialize(self._optimizer._lr_object)
+        self._initialize_optimizer()
         self._decay.decay(epoch)
+
+    def restart(self):
+        """
+        Reset the learning rate, and the learning decay itself to its original state.
+        """
+        self._initialize_optimizer()
+        self._decay.restart()
