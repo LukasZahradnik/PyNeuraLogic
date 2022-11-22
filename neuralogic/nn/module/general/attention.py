7c63bf09417cd305cfe7be5a548c62eb301af288
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2022-11-22 22:56:38 +0100

    Reimplement attention module

--- neuralogic/nn/module/general/attention.py
+++ neuralogic/nn/module/general/attention.py
@@ -1,6 +1,6 @@
 import math
 
-from neuralogic.core.constructs.function import Transformation, Combination
+from neuralogic.core.constructs.function import Transformation, Combination, Aggregation
 from neuralogic.core.constructs.factories import R
 from neuralogic.nn.module.module import Module
 
@@ -23,23 +23,25 @@ class Attention(Module):
         self.arity = arity
 
     def __call__(self):
-        terms = [f"X{i}" for i in range(1, self.arity)]
-        qterms = [f"X{i}" for i in range(self.arity)]
+        terms = [f"X{i}" for i in range(self.arity)]
+        val_terms = [f"X{i}" for i in range(self.arity - 1)]
+        agg_terms = [*val_terms, f"X{self.arity}"]
 
         d_k = 1 / math.sqrt(self.embed_dim)
 
         dk_rel = R.get(f"{self.output_name}__dk")
         dot_rel = R.get(f"{self.output_name}__dot")
+        values_rel = R.get(f"{self.output_name}__vals")
 
         return [
             dk_rel[d_k].fixed(),
-            (dot_rel(qterms) <= (dk_rel, R.get(self.key_name)(terms), R.get(self.query_name)(qterms)))
-            | [Combination.PRODUCT, Transformation.IDENTITY],
-            dot_rel / self.arity | [Transformation.IDENTITY],
-            (
-                R.get(self.output_name)(qterms)
-                <= (R.get(self.value_name)(terms).T, Transformation.SOFTMAX(dot_rel(qterms)))
-            )
+            (dot_rel(terms) <= (dk_rel, R.get(self.key_name)(agg_terms).T, R.get(self.query_name)(terms)))
+            | [Combination.PRODUCT, Transformation.IDENTITY, Aggregation.CONCAT],
+            dot_rel / self.arity | [Transformation.SOFTMAX],
+            (values_rel(val_terms) <= R.get(self.value_name)(agg_terms).T)
+            | [Transformation.IDENTITY, Aggregation.CONCAT(axis=0)],
+            values_rel / (self.arity - 1) | [Transformation.IDENTITY],
+            (R.get(self.output_name)(terms) <= (dot_rel(terms).T, values_rel(val_terms)))
             | [Transformation.IDENTITY, Combination.PRODUCT],
             R.get(self.output_name) / self.arity | [Transformation.IDENTITY],
         ]
