eba13894302dea2c17320d5b5d3c27f199a1924d
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2023-11-08 23:23:41 +0100

    Support adding Metadata to RuleBody

--- neuralogic/core/constructs/rule.py
+++ neuralogic/core/constructs/rule.py
@@ -4,10 +4,11 @@ from neuralogic.core.constructs.metadata import Metadata
 
 
 class RuleBody:
-    __slots__ = "literals"
+    __slots__ = "literals", "metadata"
 
     def __init__(self, lit1, lit2):
         self.literals = [lit1, lit2]
+        self.metadata = None
 
     def __and__(self, other):
         from neuralogic.core.constructs.relation import BaseRelation
@@ -23,6 +24,15 @@ class RuleBody:
     def __repr__(self) -> str:
         return self.__str__()
 
+    def __or__(self, other) -> "RuleBody":
+        if isinstance(other, Iterable):
+            other = Metadata.from_iterable(other)
+        elif not isinstance(other, Metadata):
+            raise NotImplementedError
+
+        self.metadata = other
+        return self
+
 
 class Rule:
     __slots__ = "head", "body", "metadata"
@@ -31,18 +41,20 @@ class Rule:
         from neuralogic.core import Relation
 
         self.head = head
+        self.metadata: Optional[Metadata] = None
 
         if head.function is not None:
             raise NotImplementedError(f"Rule head {head} cannot have a function attached")
 
         if isinstance(body, RuleBody):
+            if body.metadata is not None:
+                self._set_metadata(body.metadata)
             body = body.literals
 
         if not isinstance(body, Iterable):
             body = [body]
 
         self.body = list(body)
-        self.metadata: Optional[Metadata] = None
 
         if self.is_ellipsis_templated():
             variable_set = {term for term in head.terms if term is not Ellipsis and str(term)[0].isupper()}
@@ -105,10 +117,12 @@ class Rule:
         elif not isinstance(other, Metadata):
             raise NotImplementedError
 
-        if other.aggregation is not None and other.aggregation.rule_head_dependant():
-            other = other.copy()
-            other.aggregation = other.aggregation.process_head(self.head)
-
-        self.metadata = other
-
+        self._set_metadata(other)
         return self
+
+    def _set_metadata(self, metadata: Metadata):
+        if metadata.aggregation is not None and metadata.aggregation.rule_head_dependant():
+            metadata = metadata.copy()
+            metadata.aggregation = metadata.aggregation.process_head(self.head)
+
+        self.metadata = metadata
