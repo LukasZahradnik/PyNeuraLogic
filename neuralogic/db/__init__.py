3d431f1e2b7aa1837c56df04b4f5703b33ed46e5
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2025-01-31 00:20:06 +0100

    Move evaluation in postgres to experimental package

--- neuralogic/db/__init__.py
+++
@@ -1,5 +0,0 @@
-from neuralogic.db.converter import Converter, TableMapping
-from neuralogic.db.pg import PostgresConverter
-
-
-__all__ = ["PostgresConverter", "TableMapping", "Converter"]
---
+++ neuralogic/experimental/db/__init__.py
@@ -0,0 +1,5 @@
+from neuralogic.experimental.db.converter import Converter, TableMapping
+from neuralogic.experimental.db.pg import PostgresConverter
+
+
+__all__ = ["PostgresConverter", "TableMapping", "Converter"]
