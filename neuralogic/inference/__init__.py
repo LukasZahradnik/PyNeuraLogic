36439042788298e10fc616e377a3500ec3c7bd39
Lukáš Zahradník <LukasZahradnik96@seznam.cz>
2025-04-17 20:58:49 +0200

    Remove evaluation engines

--- neuralogic/inference/__init__.py
+++
@@ -1,4 +0,0 @@
-from neuralogic.inference.evaluation_inference_engine import EvaluationInferenceEngine
-from neuralogic.inference.inference_engine import InferenceEngine
-
-__all__ = ["InferenceEngine", "EvaluationInferenceEngine"]
